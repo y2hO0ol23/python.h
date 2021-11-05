b6646039a6d05ee4bbf80cd4c4df5fc00518e63d
yyhyuk <74889162+yyhyuk@users.noreply.github.com>
2021-11-06 00:05:51 +0900

    Update pystr.h
    make it free!

--- pystr.h
+++ pystr.h
@@ -3,16 +3,12 @@
 #include <cstring>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 namespace py {
-#define p py_str("") %
 	class pystr;
-	pystr py_str(const pystr str);
-	pystr chr(const int n);
-	int ord(pystr n);
-	int ord(const char* n);
-	int ord(const char n);
-	pystr str(int n);
+	pystr py_str(const pystr& str);
+#define p py_str("") %
 
 	class pystr {
 		friend std::ostream& operator<<(std::ostream& ret, const pystr& str);
@@ -29,43 +25,72 @@ namespace py {
 
 		void set_idx(int* idx);
 		void check_idx_out_of_range(const int idx);
+
+		bool is_over(int idx);
 	public:
 		pystr();		pystr(const char* str);		pystr(const char str);		pystr(const pystr& str);
-		char* c_str();
+		~pystr();
+		const char* c_str();
 
 		int len();
-		int count(pystr str);
-		int find(pystr str);
+		int count(const pystr& str);
+		int find(const pystr& str);
 		int index(pystr str);
 		pystr join(pystr str);
 		pystr join(std::vector<pystr> list);		pystr join(std::vector<char> list);
 		pystr upper();
 		pystr lower();
 		pystr strip();					pystr lstrip(); 				pystr rstrip();
-		pystr replace(pystr str1, pystr str2);
+		pystr replace(const pystr& target, const pystr& value);
 		std::vector<pystr> split(pystr target);		std::vector<pystr> split();
 
 		char operator[](int idx);
 		pystr operator()(int idx1, int idx2, const int jump);				pystr operator()(const char* idx1, int idx2, const int jump);		pystr operator()(int idx1, const char* idx2, const int jump);		pystr operator()(const char* idx1, const char* idx2, const int jump);
 		pystr operator()(int idx1, int idx2);						pystr operator()(const char* idx1, int idx2);				pystr operator()(int idx1, const char* idx2);				pystr operator()(const char* idx1, const char* idx2);
-		pystr operator=(const pystr& str);						pystr operator=(const char* str);					pystr operator=(const char str);
-		pystr operator+=(const pystr& str);
+		pystr& operator=(const pystr& str);						pystr& operator=(const char* str);					pystr& operator=(const char str);
+		pystr& operator+=(const pystr& str);
 		pystr operator*(const int value);
 		bool operator==(const pystr& str);
 		bool operator!=(const pystr& str);
-
+		bool operator<(const pystr& str);
+		bool operator<=(const pystr& str);
+		bool operator>(const pystr& str);
+		bool operator>=(const pystr& str);
 		pystr operator%(const pystr& str);
 	};
+	std::ostream& operator<<(std::ostream& ret, const pystr& str) {
+		ret << str.data;
+		return ret;
+	}
+	std::istream& operator>>(std::istream& ret, pystr& str) {
+		char data;
+		pystr list = p" \n" + (char)0;
+		str = "";
+		while (-1 != list.find(ret.rdbuf()->sgetc())) ret.rdbuf()->snextc();
+		while (-1 == list.find(data = ret.rdbuf()->sgetc())) {
+			str += data;
+			ret.rdbuf()->snextc();
+		}
+		return ret;
+	}
+	pystr operator+(const pystr& str1, const pystr& str2) {
+		pystr ret = str1;
+		ret += str2;
+		return ret;
+	}
+	pystr operator%(const pystr& str1, const pystr& str2) {
+		return operator+(str1, str2);
+	}
 
 	void pystr::extend_volume() {
-		set_volume(this->volume * 2);
+		this->set_volume(this->volume * 2);
 	}
 	void pystr::set_volume(const int value) {
-		this->volume = value;
 		while (1) {
-			char* temp = (char*)realloc(this->data, this->volume);
+			char* temp = (char*)realloc(this->data, sizeof(char) * value);
 			if (temp != NULL) {
 				this->data = temp;
+				this->volume = value;
 				break;
 			}
 		}
@@ -76,6 +101,10 @@ namespace py {
 	}
 	void pystr::check_idx_out_of_range(const int idx) {
 		if (idx < 0 || this->size <= idx) throw "string index out of range";
+	}
+
+	bool pystr::is_over(int idx) {
+		return this->volume <= idx + 1;
 	}
 
 	pystr::pystr() {
@@ -98,43 +127,47 @@ namespace py {
 		this->data[loop] = '\0';
 		this->size = loop;
 	}
+	pystr::~pystr() {
+		free(this->data);
+	}
 
-	char* pystr::c_str() {
-		pystr ret = *this;
-		return ret.data;
+	const char* pystr::c_str() {
+		return (const char*)(this->data);
 	}
 	int pystr::len() {
 		return this->size;
 	}
 
+	int pystr::count(const pystr& str) {
+		pystr temp = str;
 
-	int pystr::count(pystr str) {
 		int cnt = 0;
 		pystr cmp = *this;
-		int str_size = str.size;
+		int str_size = temp.size;
 		while (1) {
-			int res = cmp.find(str);
+			int res = cmp.find(temp);
 			if (res == -1) return cnt;
 			cnt++;
 			if (res + str_size >= cmp.size) return cnt;
 			cmp = cmp(res + str_size, "");
 		}
 	}
-	int pystr::find(pystr str) {
+	int pystr::find(const pystr& str) {
+		pystr temp = str;
 		//kmp algorithm
-		int str_size = str.size, j = 0;
+		int str_size = temp.size, j = 0;
 		std::vector<int> pi(str_size, 0); //get pi array
 		for (int i = 0; i < str_size; i++) {
-			while (j > 0 && str[i] != str[j]) j = pi[j - 1];
-			if (str[i] == str[j] && i != j) pi[i] = ++j;
+			while (j > 0 && temp[i] != temp[j]) j = pi[j - 1];
+			if (temp[i] == temp[j] && i != j) pi[i] = ++j;
 		}
 		//find string
 		pystr target_str = *this;
 		int target_str_size = target_str.size;
 		j = 0;
 		for (int i = 0; i < target_str_size; i++) {
-			while (j > 0 && target_str[i] != str[j]) j = pi[j - 1];
-			if (target_str[i] == str[j]) {
+			while (j > 0 && target_str[i] != temp[j]) j = pi[j - 1];
+			if (target_str[i] == temp[j]) {
 				if (j == str_size - 1) return (i - str_size + 1);
 				j++;
 			}
@@ -219,7 +252,7 @@ namespace py {
 		return ret;
 	}
 
-	pystr pystr::replace(pystr target, pystr value) {
+	pystr pystr::replace(const pystr& target, const pystr& value) {
 		pystr ret;
 		pystr str = *this;
 		int target_size = target.size;
@@ -232,10 +265,11 @@ namespace py {
 		}
 	}
 
-	std::vector<pystr> pystr::split(pystr target) {
+	std::vector<pystr> pystr::split(const pystr target) {
 		std::vector<pystr> ret;
 		pystr str = *this;
 		int target_size = target.size;
+		if (target_size == 0) throw "empty separator";
 		while (1) {
 			int res = str.find(target);
 			if (res == -1) {
@@ -248,31 +282,6 @@ namespace py {
 	}
 	std::vector<pystr> pystr::split() {
 		return this->split(' ');
-	}
-
-
-	std::ostream& operator<<(std::ostream& ret, const pystr& str) {
-		ret << str.data;
-		return ret;
-	}
-	std::istream& operator>>(std::istream& ret, pystr& str) {
-		char data;
-		pystr list = p" \n" + (char)0;
-		str = "";
-		while (-1 != list.find(ret.rdbuf()->sgetc())) ret.rdbuf()->snextc();
-		while (-1 == list.find(data = ret.rdbuf()->sgetc())) {
-			str += data;
-			ret.rdbuf()->snextc();
-		}
-		return ret;
-	}
-	pystr operator+(const pystr& str1, const pystr& str2) {
-		pystr ret = str1;
-		ret += str2;
-		return ret;
-	}
-	pystr operator%(const pystr& str1, const pystr& str2) {
-		return operator+(str1, str2);
 	}
 
 	char pystr::operator[](int idx) {
@@ -293,70 +302,70 @@ namespace py {
 	};
 	pystr pystr::operator()(const char* idx1, int idx2, const int jump) {
 		return this->operator()(0, idx2, jump);
-	};
+	}
 	pystr pystr::operator()(int idx1, const char* idx2, const int jump) {
 		return this->operator()(idx1, this->size, jump);
-	};
+	}
 	pystr pystr::operator()(const char* idx1, const char* idx2, const int jump) {
 		return this->operator()(0, this->size, jump);
-	};
+	}
 	pystr pystr::operator()(int idx1, int idx2) {
 		return this->operator()(idx1, idx2, 1);
-	};
+	}
 	pystr pystr::operator()(const char* idx1, int idx2) {
 		return this->operator()(0, idx2, 1);
-	};
+	}
 	pystr pystr::operator()(int idx1, const char* idx2) {
 		return this->operator()(idx1, this->size, 1);
-	};
+	}
 	pystr pystr::operator()(const char* idx1, const char* idx2) {
 		return this->operator()(0, this->size, 1);
-	};
+	}
 
-	pystr pystr::operator=(const pystr& str) {
+	pystr& pystr::operator=(const pystr& str) {
 		int loop = str.size;
 		for (int i = 0; i < loop; i++) {
-			if (this->volume <= i) this->extend_volume();
+			if (this->is_over(i)) this->extend_volume();
 			this->data[i] = str.data[i];
 		}
-		if (this->volume <= loop) this->extend_volume();
+		if (this->is_over(loop)) this->extend_volume();
 		this->data[loop] = '\0';
 		this->size = loop;
 		return *this;
 	}
-	pystr pystr::operator=(const char* str) {
+	pystr& pystr::operator=(const char* str) {
 		int loop = strlen(str);
 		for (int i = 0; i < loop; i++) {
-			if (this->volume <= i) this->extend_volume();
+			if (this->is_over(i)) this->extend_volume();
 			this->data[i] = str[i];
 		}
-		if (this->volume <= loop) this->extend_volume();
+		if (this->is_over(loop)) this->extend_volume();
 		this->data[loop] = '\0';
 		this->size = loop;
 		return *this;
 	}
-	pystr pystr::operator=(const char str) {
+	pystr& pystr::operator=(const char str) {
 		int point = this->size;
-		if (this->volume <= point) this->extend_volume();
+		if (this->is_over(point)) this->extend_volume();
 		this->data[point] = str;
 		this->data[point + 1] = '\0';
 		this->size = point + 1;
 		return *this;
 	}
 
-	pystr pystr::operator+=(const pystr& str) {
+	pystr& pystr::operator+=(const pystr& str) {
 		int loop = str.size;
 		int point = this->size;
 		for (int i = 0; i < loop; i++) {
-			if (this->volume <= point) this->extend_volume();
+			if (this->is_over(point)) this->extend_volume();
 			this->data[point] = str.data[i];
 			point++;
 		}
+		if (this->is_over(point)) this->extend_volume();
 		this->data[point] = '\0';
 		this->size = point;
 		return *this;
 	}
-
 	pystr pystr::operator*(int value) {
 		if (value <= 0) return py_str("");
 		pystr ret = *this;
@@ -375,51 +384,23 @@ namespace py {
 	bool pystr::operator!=(const pystr& str) {
 		return std::strcmp(this->data, str.data);
 	}
+	bool pystr::operator<(const pystr& str) {
+		return std::strcmp(this->data, str.data) < 0;
+	}
+	bool pystr::operator<=(const pystr& str) {
+		return std::strcmp(this->data, str.data) <= 0;
+	}
+	bool pystr::operator>(const pystr& str) {
+		return std::strcmp(this->data, str.data) > 0;
+	}
+	bool pystr::operator>=(const pystr& str) {
+		return std::strcmp(this->data, str.data) >= 0;
+	}
 	pystr pystr::operator%(const pystr& str) {
 		return operator+(*this, str);
 	}
-	//class end
 
-	pystr py_str(const pystr str) {
-		pystr ret(str);
-		return ret;
+	pystr py_str(const pystr& str) {
+		return str;
 	}
-
-	pystr chr(const int n) {
-		pystr ret = n;
-		return n;
-	}
-
-	int ord(pystr n) {
-		if (n.len() > 1) throw "ord() expected a character, but string of length %d found", n.len();
-		return (int)((char)n[0]);
-	}
-	int ord(const char* n) {
-		return ord(py_str(n));
-	}
-	int ord(const char n) {
-		return ord(py_str(n));
-	}
-
-	pystr str(int n) {
-		pystr ret;
-		while (n > 0) {
-			ret = chr((n % 10) + ord("0")) + ret;
-			n /= 10;
-		}
-		return ret;
-	}
-
-	//output vector<pystr>
-	std::ostream& operator<<(std::ostream& ret, const std::vector<pystr>& data) {
-		pystr res = '{';
-		int loop = data.size();
-		for (int i = 0; i < loop - 1; i++) {
-			res += "\"" + data[i] + "\", ";
-		}
-		res += "\"" + data[loop - 1] + "\"}";
-		ret << res;
-		return ret;
-	}
-
 }
