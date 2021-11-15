c460bc79dc196bc7ec9ec15afcc972ad347c4a82
yyhyuk <74889162+yyhyuk@users.noreply.github.com>
2021-11-15 17:13:47 +0900

    Update python.h

--- headerFiles/python.h
+++ headerFiles/python.h
@@ -1,32 +1,34 @@
 #pragma once
-#ifndef _PYTHOMMAIM_
-#define _PYTHOMMAIM_
-#include "pystr.h"
+#ifndef _PYTHONMAIN_
+#define _PYTHOMMAIN_
+#include "pyerr.h"
 #include "pylist.h"
+#include "pystr.h"
+#include <map>
 
 namespace py {
+#include <iostream>
 #define in :
 #define lamda [&]
+	using std::pair;
+	using std::map;
 
-	template <class T> py::pylist<std::pair<int, T>> enumerate(py::pylist<T> _List) {
-		py::pylist<std::pair<int, T>> _rList;
-		int _Lsize = _List.size();
-		for (int _Idx = 0; _Idx < _Lsize; _Idx++) {
-			_rList.append(std::make_pair(_Idx, _List[_Idx]));
+	template <class K, class V> std::ostream& operator<<(std::ostream& os, const map<K, V>& value) {
+		os << "{";
+		pylist<pystr> list;
+		for (typename map<K, V>::const_iterator iter = value.begin(); iter != value.end(); iter++) {
+			list.append(py::str(iter->first) + ":" + py::str(iter->second));
 		}
-		return _rList;
-	}
-	py::pylist<pystr> list(pystr _Str) {
-		py::pylist<pystr> _rList;
-		int size = len(_Str);
-		for (int _Idx = 0; _Idx < size; _Idx++) {
-			_rList.append(_Str[_Idx]);
+		int length = py::len(list);
+		if(length > 0) os << *list[0];
+		for (int idx = 1; idx < length; idx++) {
+			os << ", " << *list[idx];
 		}
-		return _rList;
+		return os << "}";
 	}
 	//has err
-	/*template <typename T, typename K> py::pylist<K> map(std::function<K> _Func, py::pylist<T> _List) {
-		py::pylist<T> _rList;
+	/*template <typename T, typename K> pylist<K> map(std::function<K> _Func, pylist<T> _List) {
+		pylist<T> _rList;
 		int _Lsize = _List.len();
 		for (int _Idx = 0; _Idx < _Lsize; _Idx++) {
 			_rList.append(_Func(_List[_Idx]));
@@ -34,25 +36,53 @@ namespace py {
 		return _rList;
 	}*/
 	/**/
-	py::pylist<int> range(int _Begin, int _End, int _Jmp = 1) {
-		py::pylist<int> _rList;
-		if ((_End - _Begin) * _Jmp < 0) return _rList;
-		if (_Jmp == 0) throw "range() arg 3 must not be zero"p;
+	/*template<class T>
+	void print(T argc, ...) {
+		va_list ap;
 
-		int _Flag = 1;
-		if (_Jmp < 0) {
-			_Flag *= -1;
-			_Begin *= -1;
-			_End *= -1;
-			_Jmp *= -1;
+		va_start(ap, argc);
+		for (int _ = 0; _ < argc; _++) {
+			auto num = va_arg(ap, auto);    
+			std::cout << str(num);
 		}
-		for (int _Val = _Begin; _Val < _End; _Val += _Jmp) {
-			_rList.append(_Val * _Flag);
+		va_end(ap);
+	}*/
+
+	template <class T> pylist<pair<int, T>> enumerate(pylist<T> value) {
+		pylist<pair<int, T>> ret_value;
+		int size = py::len(value);
+		for (int idx = 0; idx < size; idx++) {
+			ret_value.append(std::make_pair(idx, value[idx]));
 		}
-		return _rList;
+		return ret_value;
 	}
-	py::pylist<int> range(const int _End) {
-		return range(0, _End);
+	pylist<pystr> list(pystr value) {
+		pylist<pystr> ret_value;
+		int size = py::len(value);
+		for (int _Idx = 0; _Idx < size; _Idx++) {
+			ret_value.append(value[_Idx]);
+		}
+		return ret_value;
+	}
+	pylist<int> range(const int start, const int stop, const int step = 1) {
+		if (step == 0) ValueError("range() arg 3 must not be zero");
+		pylist<int> ret_value;
+		if ((start - stop) * step < 0) return ret_value;
+
+		int s = 1;
+		int value = start;
+		if (step < 0) {
+			s *= -1;
+			value *= -1;
+		}
+		while (value < stop * s) {
+			ret_value.append(value * s);
+			value += step * s;
+		}
+		return ret_value;
+	}
+	pylist<int> range(const int stop) {
+		return range(0, stop);
 	}
 }
 #endif
